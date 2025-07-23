7e03216b91890fe9710109d5de7554a4acf16b2b
Diogo Goncalves <oggoyd@gmail.com>
2025-07-23 20:05:42 -0300

    Incluindo uma classe principal para controlar menu e refatorando o codigo

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,47 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private static final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private static final String API_KEY = "&apikey=6585022c";
+    public static final String TEMPORADA = "&season=";
+    private Scanner scanner = new Scanner(System.in);
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    public void exibeMenu() {
+
+        // Obtendo o nome da série
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = scanner.nextLine();
+
+        // Consultando dados da série
+        String nomeFormatado = nomeSerie.replace(" ", "+");
+        var json = consumoApi.obterDados(ENDERECO + nomeFormatado + API_KEY);
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        // Consultando dados das temporadas
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+            json = consumoApi.obterDados(ENDERECO + nomeFormatado + TEMPORADA + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
+
+
+    }
+
+}
